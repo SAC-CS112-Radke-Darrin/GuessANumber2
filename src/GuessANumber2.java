0909d180e9c7f8e610f4fac9bac567567068aae7
darrin3radke@gmail.com <darrin3radke@gmail.com>
2015-10-07 21:01:19 -0700

    Week 7, Lab 1

--- src/GuessANumber2.java
+++ src/GuessANumber2.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class GuessANumber2 {
@@ -7,28 +8,23 @@ public class GuessANumber2 {
 		// TODO Auto-generated method stub
 		Scanner inp = new Scanner(System.in);
 		
-		int cmpNum, usrNum;
-		String ppt;
-		cmpNum = 1 + (int)(Math.random()*10);
-		System.out.printf("%d\n", cmpNum);
+		int cmpNum;
+		int usrNum;
+		String usrInp;
 		
-			do {
+		cmpNum = 1 + (int)(Math.random()*10);
 				
-				usrNum = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 1 & 10."));
-				if (usrNum > cmpNum){
-					JOptionPane.showMessageDialog(null, "Too big.");}
-				else if (usrNum < cmpNum){
-					JOptionPane.showMessageDialog(null, "Too small.");}
-				else if (usrNum == cmpNum){
-					JOptionPane.showMessageDialog(null, "Match.");}
-				else if (usrNum != cmpNum)
-					ppt = JOptionPane.showInputDialog("Continue? (y/n)");}
-			
-			while ((ppt == "y") && (usrNum != cmpNum));
-				ppt = inp.next();
-			
-			if ((ppt == "n"))
-				System.out.println("End.");
+		usrInp = JOptionPane.showInputDialog("Guess a number between 1 & 10.");
+		usrNum = Integer.parseInt(usrInp);
+				
+		if (usrNum > cmpNum){
+			JOptionPane.showMessageDialog(null, "Too big.");}
+		else if (usrNum < cmpNum){
+			JOptionPane.showMessageDialog(null, "Too small.");}
+		else if (usrNum == cmpNum){
+			JOptionPane.showMessageDialog(null, "Match.");}
+		
+		JOptionPane.showMessageDialog(null, "End.");
 		
 		inp.close();
 	}
