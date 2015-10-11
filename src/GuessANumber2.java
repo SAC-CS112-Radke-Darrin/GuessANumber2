13d4fd5f60102f7a7627e5ef1ce3171305ad3ee1
SAC-CS112-Radke-Darrin <darrin3radke@gmail.com>
2015-10-11 15:00:12 -0700

    Week 6, Lab 8

--- src/GuessANumber2.java
+++ src/GuessANumber2.java
@@ -6,27 +6,35 @@ public class GuessANumber2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Scanner inp = new Scanner(System.in);
-		
-		int cmpNum;
+						
+		int cmpNum = 1 + (int)(Math.random()*10);
 		int usrNum;
-		String usrInp;
 		
-		cmpNum = 1 + (int)(Math.random()*10);
+		do
+			{
+			String usrInp = JOptionPane.showInputDialog("Guess a number between 1 & 10.");
+			usrNum = Integer.parseInt(usrInp);
 				
-		usrInp = JOptionPane.showInputDialog("Guess a number between 1 & 10.");
-		usrNum = Integer.parseInt(usrInp);
-				
-		if (usrNum > cmpNum){
-			JOptionPane.showMessageDialog(null, "Too big.");}
-		else if (usrNum < cmpNum){
-			JOptionPane.showMessageDialog(null, "Too small.");}
-		else if (usrNum == cmpNum){
-			JOptionPane.showMessageDialog(null, "Match.");}
+			if (usrNum > cmpNum)
+			{
+				JOptionPane.showMessageDialog(null, "Too big.");
+				}
+			else if (usrNum < cmpNum)
+			{
+				JOptionPane.showMessageDialog(null, "Too small.");
+				}
+			else if (usrNum == cmpNum)
+			{
+				JOptionPane.showMessageDialog(null, "Match.");
+				}
+			}
+		while (usrNum != cmpNum);
+						
+	JOptionPane.showMessageDialog(null, "End.");
 		
-		JOptionPane.showMessageDialog(null, "End.");
-		
-		inp.close();
+	inp.close();
 	}
 
 }
